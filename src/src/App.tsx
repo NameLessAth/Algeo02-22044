b868ca89c316159c7fbcf4ace349687c41663b9c
Julian Caleb Simandjuntak <juliancaleb88@gmail.com>
2023-11-02 08:03:36 +0700

    feat: image uploader

---
+++ src/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageUpload from './component/ImageUpload';
+import Title from './component/Title';
+
+function App() {
+  return (
+    <>
+      <Title />
+      <ImageUpload />
+    </>
+  );  
+}
+
+export default App;
