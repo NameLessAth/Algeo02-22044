b1a5c8c5bc28e95319703d05e366d8bf221362c2
Julian Caleb Simandjuntak <juliancaleb88@gmail.com>
2023-11-04 06:28:13 +0700

    feat: gallery

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ImageUpload from './component/ImageUpload';
 import Title from './component/Title';
+import ImageGallery from './component/ImageGallery';
 
 function App() {
   return (
     <>
       <Title />
       <ImageUpload />
+      <ImageGallery />
     </>
   );  
 }
